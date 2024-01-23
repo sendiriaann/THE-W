6f6510dc9ba16ff491ea8df0b28fd2849f6fa1df
Sendirian <mechadroid1@gmail.com>
2024-01-23 17:02:49 +0700

    Tried and Tested Trust Plz

---
+++ translate.js
@@ -0,0 +1,16 @@
+const translate = document.querySelectorAll(".translate");
+const header = document.querySelector("header");
+const section = document.querySelector("section");
+
+let header_height = header.offsetHeight;
+let section_height = section.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  let scroll = window.pageYOffset;
+  let sectionY = section.getBoundingClientRect();
+
+  translate.forEach((element) => {
+    let speed = element.dataset.speed;
+    element.style.transform = `translateY(${scroll * speed}px)`;
+  });
+});
