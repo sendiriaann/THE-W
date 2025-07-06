41576d755bbca326edda60ef6209121a0462e891
sendirian <mechadroid1@gmail.com>
2025-07-06 08:03:26 +0700

    Update doNOT.js

--- RickRollReal/doNOT.js
+++ RickRollReal/doNOT.js
@@ -17,6 +17,6 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   
 
-// window.onload = function() {
-//     document.getElementById("myAudio").play();
-// }
+window.onload = function() {
+    document.getElementById("myAudio").play();
+}
